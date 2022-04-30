34f4457044468adf81ed92ca2b294b102c19a651
coranos <coranos@users.noreply.github.com>
2022-04-29 21:09:31 -0500

    fixing ledgercomm tests

--- tests/ledgercomm/boilerplate_client/transaction.py
+++ tests/ledgercomm/boilerplate_client/transaction.py
@@ -10,29 +10,14 @@ class TransactionError(Exception):
 
 
 class Transaction:
-    def __init__(self, nexus: str, chain: str, script: str) -> None:
-        self.nexus: bytes = bytes.fromhex(nexus[2:]) if isinstance(nexus, str) else nexus
-        self.chain: bytes = bytes.fromhex(chain[2:]) if isinstance(chain, str) else chain
-        self.script: bytes = bytes.fromhex(script[2:]) if isinstance(script, str) else script
+    def __init__(self, tx_hex: str) -> None:
+        self.tx_hex: str = tx_hex
 
     def serialize(self) -> bytes:
-        return b"".join([
-            write_varint(len(self.nexus)),
-            self.nexus,
-            write_varint(len(self.chain)),
-            self.chain,
-            write_varint(len(self.script)),
-            self.script,
-        ])
+        return bytes.fromhex(self.tx_hex)
 
     @classmethod
     def from_bytes(cls, hexa: Union[bytes, BytesIO]):
         buf: BytesIO = BytesIO(hexa) if isinstance(hexa, bytes) else hexa
-        nexus_len: int = read_varint(buf)
-        nexus: str = read(buf, nexus_len)
-        chain_len: int = read_varint(buf)
-        chain: str = read(buf, chain_len)
-        script_len: int = read_varint(buf)
-        script: str = read(buf, script_len)
-
-        return cls(nexus=nexus,chain=chain,script=script)
+        tx_hex: str = buf.hex();
+        return cls(tx_hex=tx_hex)
